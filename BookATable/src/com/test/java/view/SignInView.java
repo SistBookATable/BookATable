3945ddb3cfb5de9e394abfebdc0599823cca6168
kimhyeokjoon <fufu98@naver.com>
2024-01-31 15:15:05 +0900

    [docs] 회원가입 기능 주석 추가

--- BookATable/src/com/test/java/view/SignInView.java
+++ BookATable/src/com/test/java/view/SignInView.java
@@ -5,33 +5,10 @@ import java.util.Scanner;
 
 public class SignInView {
 
+
 	/**
-	 * 회원 가입 정보 입력
-	 * @return ArrayList<String>
-	 * {이름,주민번호,전화번호,계좌번호,id,pw}
+	 * 일반회원가입, 업체회원가입 선택 화면 
 	 */
-	public static ArrayList<String> getInput() {
-		
-		Scanner scan = new Scanner(System.in);
-		ArrayList<String> temp = new ArrayList<>();
-
-		System.out.print("이름 : ");
-		temp.add(scan.nextLine());
-		System.out.print("주민번호 : ");
-		temp.add(scan.nextLine());
-		System.out.print("전화번호 : ");
-		temp.add(scan.nextLine());
-		System.out.print("계좌번호 : ");
-		temp.add(scan.nextLine());
-		System.out.print("ID : ");
-		temp.add(scan.nextLine());
-		System.out.print("PW : ");
-		temp.add(scan.nextLine());
-		
-		
-		return temp;
-	}
-
 	public static void showSelectType() {
 		System.out.println("1. 일반 회원");
 		System.out.println("2. 업체 회원");
@@ -40,6 +17,10 @@ public class SignInView {
 		System.out.print("선택(번호) : ");
 	}
 
+	/**
+	 * @return
+	 * 선택한 번호 반환
+	 */
 	public static int getSelectType() {
 		Scanner scan = new Scanner(System.in);
 		int tmp = scan.nextInt();

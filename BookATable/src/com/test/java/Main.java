81e1c95acdddb248434d1a208e4b4523de62399b
kimhyeokjoon <fufu98@naver.com>
2024-01-31 10:59:54 +0900

    [docs] model 객체 생성

--- BookATable/src/com/test/java/Main.java
+++ BookATable/src/com/test/java/Main.java
@@ -5,8 +5,7 @@ import com.test.java.controller.MainController;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		MainController.start();
+		MainController.start();  
 	}
 
 }
---
+++ BookATable/src/com/test/java/model/Review.java
@@ -0,0 +1,15 @@
+package com.test.java.model;
+
+import java.util.Calendar;
+
+public class Review {
+	private int reviewNumber;
+	private Calendar dateWritten;
+	private String content;
+	private double score;
+	private int reservationNumber;
+	
+	public boolean checkNumber(int num) {
+		return false;
+	} 
+}

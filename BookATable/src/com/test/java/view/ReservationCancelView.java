dbe5f5ccb50d42562134ecbf5f4c208a11d792ff
zzzzzzin <jyj7314@naver.com>
2024-02-01 23:59:12 +0900

    [docs]logIn 해당아이디 없을때 다시 로그인할건지 메인페이지로 돌아갈건지 추가

--- BookATable/src/com/test/java/view/ReservationCancelView.java
+++ BookATable/src/com/test/java/view/ReservationCancelView.java
@@ -10,7 +10,7 @@ public void showReservationCancel() {
 		System.out.println("           예약을 취소하시겠습니까? (Y/N)		   ");
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------");
-		System.out.print("입력(Y/N)");
+		System.out.print("입력(Y/N): ");
 	}
 
 	public String get() {
@@ -21,7 +21,7 @@ public void showReservationCancel() {
 	}
 
 	public void isCanCel() {
-		
+		//TODO 예약취소가 어떻게 되는건지,,? 예약 내역 중에 취소할 예약의 상호명을 적으면 해당 예약을 취소,,??
 	}
 
 }
--- BookATable/src/com/test/java/view/ReservationStatusInquiryView.java
+++ BookATable/src/com/test/java/view/ReservationStatusInquiryView.java
@@ -9,14 +9,14 @@ public class ReservationStatusInquiryView {
 
 		System.out.println("------------------------------------------------------------------------------------------");
 		System.out.println();
-		System.out.printf("■           %s님 현재 예약 현황		   ■", name);
+		System.out.printf("■           %s님 현재 예약 현황		   ■\n", name);
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------");
 
 		System.out.println();
 
-		System.out.printf("[상호명]\t[인원수]\t[예약 시간]");
-		System.out.printf("맥도날드\t3명\t13:00\t");
+		System.out.printf("[상호명]\t[인원수]\t[예약 시간]\n");
+		System.out.printf("맥도날드\t3명\t13:00\t\n");
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------");
 		System.out.println();

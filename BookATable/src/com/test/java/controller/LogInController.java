aa33c167e2e7e3957501ebddfe8034771ba1440e
zin <88925213+zzzzzzin@users.noreply.github.com>
2024-02-01 11:21:18 +0900

    [docs]login 기능 완료

--- BookATable/src/com/test/java/controller/LogInController.java
+++ BookATable/src/com/test/java/controller/LogInController.java
@@ -7,6 +7,7 @@ import com.test.java.model.Member;
 import com.test.java.model.User;
 import com.test.java.repository.Data;
 import com.test.java.view.LogInView;
+import com.test.java.view.View;
 
 public class LogInController {
 
@@ -20,43 +21,68 @@ public class LogInController {
 			String inputId = input.get(0);
 			String inputPw = input.get(1);
 
-			if (checkInputId(inputId) == null) {
-				System.out.println("해당 아이디를 찾을 수 없습니다.");
-				loop = true;
-			} else {
-				if (checkInputPw(inputPw) == null ) {
-					System.out.println("비밀번호가 맞지 않습니다.");
-				}
-			}
+//			if (checkInputId(inputId) == null) {
+//				System.out.println("해당 아이디를 찾을 수 없습니다.");
+//				View.pause();
+//				continue;
+//			}	
+//			if (checkInputPw(inputId,inputPw) == null ) {
+//				System.out.println("비밀번호가 맞지 않습니다.");
+//				View.pause();
+//				continue;
+//			} else {
+//				System.out.printf("");
+//				loop = false;
+//			}
+			
+			System.out.println(checkLogIn(inputId, inputPw)); 
+			View.pause();   
 
-			System.out.println("등록된 아이디입니다.");
-			loop = false;
 		}
 
 	}
 
-	private String checkInputPw(String inputPw) {
-
+	private String checkLogIn(String inputId, String inputPw) {
+		String result = "";
 		for (Member u : Data.memberList) {
-
-			if (inputPw.equals(u.getPw())) {
-				return u.getName();
-			}
-		}
-
-		return null;
-	}
-
-	private String checkInputId(String inputId) {
-
-		for (Member u : Data.memberList) {
-
 			if (inputId.equals(u.getId())) {
-				return u.getName();
-			}
+				if (!inputPw.equals(u.getPw())) {
+				result = "입력한 비밀번호가 올바르지 않습니다.";
+				return result;
+				} else {
+					result = u.getName() + "님, 로그인에 성공하셨습니다.";
+					Member.level = u.getUserType();
+					return result;
+				}
+			} 
 		}
 
-		return null;
+		result = "해당 아이디를 찾을 수 없습니다.";
+		return result;
 	}
+
+//	private String checkInputPw(String inputId, String inputPw) {
+//
+//		for (Member u : Data.memberList) {
+//
+//			if (inputPw.equals(u.getPw()) && inputId.equals(u.getId())) {
+//				return u.getName();
+//			}
+//		}
+//
+//		return null;
+//	}
+//
+//	private String checkInputId(String inputId) {
+//
+//		for (Member u : Data.memberList) {
+//
+//			if (inputId.equals(u.getId())) {
+//				return u.getName();
+//			}
+//		}
+//
+//		return null;
+//	}
 
 }

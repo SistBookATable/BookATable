25b2cfd66df33a135baaf7e0d58d2524a1afcf3f
zin <88925213+zzzzzzin@users.noreply.github.com>
2024-02-02 10:26:48 +0900

    일반회원 > 마이페이지 > 예약관리 > 예약현황조회 추가

--- BookATable/src/com/test/java/view/ReservationStatusInquiryView.java
+++ BookATable/src/com/test/java/view/ReservationStatusInquiryView.java
@@ -1,22 +1,36 @@
 package com.test.java.view;
 
+import java.util.Iterator;
 import java.util.Scanner;
+
+import com.test.java.model.Member;
+import com.test.java.model.Reservation;
+import com.test.java.model.Store;
+import com.test.java.repository.Data;
 
 public class ReservationStatusInquiryView {
 	
 	
-	public void showReservationStatusInquiry(String name) {
+	public void showReservationStatusInquiry() {
 
 		System.out.println("------------------------------------------------------------------------------------------");
 		System.out.println();
-		System.out.printf("■           %s님 현재 예약 현황		   ■", name);
+		System.out.printf("■           %s님 현재 예약 현황		   ■", findNameById(Member.id));
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------");
 
 		System.out.println();
-
 		System.out.printf("[상호명]\t[인원수]\t[예약 시간]");
-		System.out.printf("맥도날드\t3명\t13:00\t");
+		
+		while(true) {
+			for(Reservation r : Data.reservationList) {
+				if (r.getState().equals("예약")) {
+					System.out.printf("%s\t%d명\t%s\t\n",findStoreNameById(Member.id), findNumOfPepleById(Member.id), findreservationTimeById(Member.id));
+				}
+			}
+			break;
+		}
+		
 		System.out.println();
 		System.out.println("------------------------------------------------------------------------------------------");
 		System.out.println();
@@ -31,6 +45,46 @@ public class ReservationStatusInquiryView {
 		System.out.println();
 	}
 
+	private String findreservationTimeById(String id) {
+		String reservationTime = "";
+		for(Reservation r : Data.reservationList) {
+			if (r.getUserId().equals(id)) {
+				reservationTime = r.getReservationTime();
+			}
+		}
+		
+		return reservationTime;
+	}
+
+	private int findNumOfPepleById(String id) {
+		int numOfPeple = 0;
+		
+		for(Reservation r : Data.reservationList) {
+			if (r.getUserId().equals(id)) {
+				numOfPeple = r.getNumOfPeple();
+			}
+		}
+		
+		return numOfPeple;
+	}
+
+	private String findStoreNameById(String id) {
+		String storeName = "";
+		for(Reservation r : Data.reservationList) {
+			if (r.getUserId().equals(id)) {
+				for(Store s : Data.storeList) {
+					if (r.getLicenseNumber().equals(s.getStoreName())) {
+						storeName = s.getStoreName();
+					}
+				}
+				
+			}
+		
+		}
+		
+		return storeName;
+	}
+
 	public static int get() {
 		Scanner scan = new Scanner(System.in);
 		int temp = scan.nextInt();
@@ -38,7 +92,21 @@ public class ReservationStatusInquiryView {
 		
 		return temp;
 	}
+
+	private String findNameById(String id) {
+		String name = "";
+
+		for(Member u : Data.memberList) {
+			if(u.getId().equals(id)) {
+				name = u.getName();
+				break;
+			}
+		}
+		return name;
+		
+	}
 	
 	
+
 
 }

828233fcb2f631de608a5f466a89221e2975ee99
zin <88925213+zzzzzzin@users.noreply.github.com>
2024-02-02 15:43:37 +0900

    [docs]이용완료 예약 조회 추가

--- BookATable/src/com/test/java/controller/ReservationManagementController.java
+++ BookATable/src/com/test/java/controller/ReservationManagementController.java
@@ -3,37 +3,45 @@ package com.test.java.controller;
 import com.test.java.model.Member;
 import com.test.java.repository.Data;
 import com.test.java.view.InquiryReservationStatusView;
+import com.test.java.view.ReservationManagementView;
 
 public class ReservationManagementController {
 
-	public void reservationStatusInquiry() {
-		InquiryReservationStatusView reservationStatusInquiryView
-			= new InquiryReservationStatusView();
-		reservationStatusInquiryView.showInquiryReservationStatus();
-		
+	public void reservationManagement() {
+
+		ReservationManagementView reservationManagementView = new ReservationManagementView();
+		reservationManagementView.showReservationManagement();
+
 		int choice = InquiryReservationStatusView.get();
-		
-		switch(choice) {
+
+		switch (choice) {
 		case 1:
-			ReservationCancelController reservationCancelController
-				= new ReservationCancelController();
-			reservationCancelController.reservationCancel();
+			InquiryReservationStatusController inquiryReservationStatusController = new InquiryReservationStatusController();
+			inquiryReservationStatusController.inquiryReservationStatus();
+			break;
+		case 2:
+			InquiryCompletedReservationController inquiryCompletedReservationController
+				= new InquiryCompletedReservationController();
+			inquiryCompletedReservationController.inquiryCompletedReservation();
+			break;
 		case 0:
-			
+			break;
+
 		}
+
 	}
 
 	private String findNameById(String id) {
 		String name = "";
 
-		for(Member u : Data.memberList) {
-			if(u.getId().equals(id)) {
+		for (Member u : Data.memberList) {
+			if (u.getId().equals(id)) {
 				name = u.getName();
 				break;
 			}
 		}
 		return name;
-		
+
 	}
-	
+
 }

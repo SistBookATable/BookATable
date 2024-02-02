8ae3e7213edf120912eef1688705cef8bfe33bed
zyoo-d <ljiyxx96@gmail.com>
2024-02-02 12:36:03 +0900

    [docs] Data.java - loadReservation() 수정

--- BookATable/src/com/test/java/repository/Data.java
+++ BookATable/src/com/test/java/repository/Data.java
@@ -82,12 +82,14 @@ public class Data {
 		while((line = reader.readLine())!=null){
 			
 			String[] tmp = line.split(",");
-			ArrayList<String> arr = new ArrayList<>();
+			ArrayList<String> bookedMenuList = new ArrayList<>();
 			for(int i=0; i<Integer.parseInt(tmp[5]); i++) {
-				arr.add(tmp[i+8]);
+				bookedMenuList.add(tmp[i+8]);
 			}
+			
 			Reservation reservation = new Reservation(Integer.parseInt(tmp[0]),tmp[1],tmp[2],tmp[3],tmp[4]
-													,Integer.parseInt(tmp[5]),Integer.parseInt(tmp[6]),tmp[7], arr);
+													,Integer.parseInt(tmp[5]),Integer.parseInt(tmp[6]),tmp[7], bookedMenuList);
+			
 			reservationList.add(reservation);
 		}
 
@@ -265,10 +267,9 @@ public class Data {
 			
 			while((line = reader.readLine()) != null) {
 				
-//				String[] tmp = line.split(",");
-////				 TODO: model 생성자 수정후 변경하기
-//				BusinessUser businessUser = new BusinessUser(Integer.parseInt(tmp[0]), tmp[1], tmp[2], tmp[3]
-//															, tmp[4], tmp[5], tmp[6],  tmp[7], tmp[8], tmp[9]);
+				String[] tmp = line.split(",");
+				BusinessUser businessUser = new BusinessUser(Integer.parseInt(tmp[0]), tmp[1], tmp[2], tmp[3]
+															, tmp[4], tmp[5], tmp[6],  tmp[7], tmp[8], tmp[9]);
 				
 				memberList.add(businessUser);
 			}

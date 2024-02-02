5a5c35681d421baa8209fa4098132aec19ad6d4e
zyoo-d <ljiyxx96@gmail.com>
2024-02-02 10:56:37 +0900

    [docs] Menu.java 생성자생성, Data.java loadMenu(), saveMenu() 생성

--- BookATable/src/com/test/java/model/Menu.java
+++ BookATable/src/com/test/java/model/Menu.java
@@ -1,11 +1,22 @@
 package com.test.java.model;
 
 public class Menu {
+	
 	private String licenseNumber;
 	private String menuName;
 	private int price;
 	
-	public void changeMenu(String licenseNumbe,String menuName, int price) {
+
+
+	public Menu(String licenseNumber, String menuName, int price) {
+		super();
+		this.licenseNumber = licenseNumber;
+		this.menuName = menuName;
+		this.price = price;
+	}
+
+
+	public void changeMenu(String licenseNumber,String menuName, int price) {
 		this.menuName = menuName;
 		this.price = price;
 	}

186ec0c30caf04d7a1e1c3700b760a6753b7af1a
zyoo-d <ljiyxx96@gmail.com>
2024-02-02 14:21:09 +0900

    [docs] blackList.java 생성자생성, dat\stroe.txt 수정

--- BookATable/src/com/test/java/model/BlackList.java
+++ BookATable/src/com/test/java/model/BlackList.java
@@ -4,4 +4,28 @@ public class BlackList {
 	private String licenseNumber;
 	private String UserId;
 	private int noShowCount;
+	
+	public BlackList(String licenseNumber, String userId, int noShowCount) {
+		super();
+		this.licenseNumber = licenseNumber;
+		UserId = userId;
+		this.noShowCount = noShowCount;
+	}
+	
+	public void getLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+	public void getUserId(String UserId) {
+		this.UserId = UserId;
+	}
+	public void getNoShowCount(int noShowCount) {
+		this.noShowCount = noShowCount;
+	}
+	
+	@Override
+	public String toString() {
+		return "BlackList [licenseNumber=" + licenseNumber + ", UserId=" + UserId + ", noShowCount=" + noShowCount
+				+ "]";
+	}
+	
 }

a95083b06b9360f6c49afd77b67ff16435d80611
HyeokJoon <fufu98@naver.com>
2024-02-06 15:19:47 +0900

    [docs] 리뷰 삭제 요청 기능 구현

--- BookATable/src/com/test/java/repository/RequestRepository.java
+++ BookATable/src/com/test/java/repository/RequestRepository.java
@@ -1,13 +1,22 @@
 package com.test.java.repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import com.test.java.model.Request;
 
 public class RequestRepository {
 
+	
 	public static ArrayList<Request> FindAllRequest() {
 		return Data.requestList;
+	}
+	
+	public static void addRequest(int reviewNumber, String licenseNumber, String reason, String requester) {
+		Calendar cur = Calendar.getInstance();
+		String today = String.format("%tF", cur);
+		Request tmp = new Request(0, reviewNumber, licenseNumber, reason, requester, today, null);
+		Data.requestList.add(tmp);
 	}
 
 }

b5613ab63b3c4df8b8acdacc05f0d57c7c61d798
zyoo-d <ljiyxx96@gmail.com>
2024-02-13 23:01:45 +0900

    [docs] FaqController , FaqView 개발주석 작성

--- BookATable/src/com/test/java/view/FaqView.java
+++ BookATable/src/com/test/java/view/FaqView.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 import com.test.java.controller.FaqController;
 import com.test.java.controller.UserMyPageController;
 import com.test.java.model.Faq;
-
+/**
+* FaqView 클래스는 FAQ 관련 기능을 담당하는 뷰 클래스입니다.
+* 사용자에게 FAQ 목록을 보여주고, 선택한 FAQ에 대한 내용을 출력합니다.
+* 또한 이전 화면으로 이동하거나 MyPage로 이동할 수 있는 기능을 제공합니다.
+*/
 public class FaqView {
 
+	/**
+	 * FAQ의 메인화면을 출력하는 메서드입니다.
+	 */
 	public static void showFaq() {
 		System.out.println();
 		System.out.println();
-//		System.out.println("‖‗▶=¦|⌈⌉⌊⌋—–-−¯");
-		System.out.println("‖¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯‖");
+		System.out.println("‖¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯‖");
 		System.out.println("‖			    FAQ                         ‖");
 		System.out.println("‖‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‖");
 		System.out.println();
@@ -31,6 +37,10 @@ public class FaqView {
 		
 	}
 	
+	/**
+	 * 회원에게 조회할 FAQ번호를 입력받는 메서드입니다.
+	 * @return 회원이 입력한 FAQ번호
+	 */
 	public static int getSelectNumber() {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("선택(번호) : ");
@@ -39,20 +49,35 @@ public class FaqView {
 	}
 	
 
-	public static void showFaqBySelectNum(Faq f) {
+	/**
+	 * 선택한 FAQ 번호에 해당하는 FAQ를 화면에 출력합니다.
+	 * @param selectNum 선택한 FAQ 객체
+	 */
+	public static void showFaqBySelectNum(Faq selectNum) {
 		
-		Faq selectFaq =f;
+		// 선택한 FAQ 객체를 selectFaq 변수에 저장합니다.
+		Faq selectFaq =selectNum;
+		
+		// 질문과 답변을 가져옵니다.
 		String questionList = selectFaq.getQuestion();
 		String answerList = selectFaq.getAnswer();
+		
+		// 질문과 답변을 배열로 분리합니다.
 		String[] question = questionList.split("\\.");
 		String[] answer = answerList.split("\\.");
 		System.out.println();
+		
+		// FAQ 제목을 출력합니다.
 		System.out.printf("%s\n",selectFaq.getTitle());
 		System.out.println("========================================================");
+		
+		// 질문을 출력합니다.
 		for(int i = 0 ; i < question.length; i++) {
 			System.out.printf("%s\n",question[i]);
 		}
 		System.out.println("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+		
+		// 답변을 출력합니다.
 		for(int i = 0 ; i < answer.length; i++) {
 			System.out.printf("%s\n",answer[i]);
 		}
@@ -61,13 +86,13 @@ public class FaqView {
 		System.out.println("1. MyPage");
 		System.out.println("0. 이전 화면");
 		
-		
+		// 반복문을 통해 사용자 입력을 받고 처리합니다.
 		boolean loop = true;
 		FaqController faq = new FaqController();
 		UserMyPageController userMyPage = new UserMyPageController();
 		
 		while(true) {
-			int num = getSelectNumber();
+			int num = getSelectNumber(); // 사용자에게 번호를 입력 받는 메서드
 			
 			switch(num) {
 			//1. MyPage로 이동

a3acca1f2614cc4fa99fdd383986bbe87cf54333
wonhyuk21 <wonhyuk2101@gmail.com>
2024-02-14 00:12:52 +0900

    [docs]회원탈퇴,상세페이지즐겨찾기,음식점검색주석달기

--- BookATable/src/com/test/java/controller/MembershipWithdrawalController.java
+++ BookATable/src/com/test/java/controller/MembershipWithdrawalController.java
@@ -5,45 +5,48 @@ import java.util.Iterator;
 
 import com.test.java.model.BusinessUser;
 import com.test.java.model.Member;
-import com.test.java.model.MembershipWithdrawal;
 import com.test.java.repository.Data;
 import com.test.java.view.MainView;
 import com.test.java.view.MembershipWithdrawalView;
 
+/**
+ * 회원탈퇴 기능을 제공하는 MembershipWithdrawalController 클래스
+ */
 public class MembershipWithdrawalController {
 
-	private MembershipWithdrawal membershipWithdrawal;
-	private BusinessUser businessUser;
 	private MembershipWithdrawalView membershipWithdrawalView;
 	LogOutController logOutController = new LogOutController();
-	ArrayList<BusinessUser> pw = new ArrayList<BusinessUser>();
 	
+	/**
+	 * MembershipWithdrawalController의 생성자
+	 */
 	public MembershipWithdrawalController() {
 		this.membershipWithdrawalView = new MembershipWithdrawalView();
 	}
 	
+	/**
+	 * 회원탈퇴 후 회원을 제거하고 로그아웃을 하는 membershipWithdrawal 메서드
+	 * 회원이 탈퇴할 때 list의 요소를 제거해야 하므로 iterator를 사용하여 다음 요소가 있는지 확인 후
+	 * 회원탈퇴 시 입력한 비밀번호와 memberList의 비밀번호와 일치한 값이 있다면 회원을 삭제하고 logOut 시킴
+	 */
 	public void membershipWithdrawal() {
 		membershipWithdrawalView.showdrawalLogo();
 
 		boolean loop = true;
 		while(loop) {
-
 			membershipWithdrawalView.inputInfo();
 			String inputPassword = membershipWithdrawalView.get();
-			//String businessPw = businessUser.getPw();
 			for(Iterator<Member> itr = Data.memberList.iterator(); itr.hasNext();) {	// Data.memberList의 iterator() 생성
 				Member b = itr.next();													// itr의 다음요소 존재여부 확인
-				if(b.getId().equals(Member.id)) {   		// id돌면서					// itr의 다음요소를 확인 후 b에 넣음	
-					if(b.getPw().equals(inputPassword)) {	// member.id가 로그인하면서 계속 돔
+				if(b.getId().equals(Member.id)) {   									// itr의 다음요소를 확인 후 b에 넣음	
+					if(b.getPw().equals(inputPassword)) {	
 						itr.remove();
 						membershipWithdrawalView.withDrawal();
 						logOutController.logOut();
-						
+						// false가 되면 해당 메서드를 호출한 곳으로 이동
 						loop = false;   
 					} else {
 						membershipWithdrawalView.noneWithDrawal();
-						//membershipWithdrawalView.backPage();
-						//membershipWithdrawalView.get();
 						break;
 					}
 				}
@@ -51,27 +54,3 @@ public class MembershipWithdrawalController {
 		}
 	}
 }
-//         for(Member b : Data.memberList) {
-//            if(b.getId().equals(Member.id)) {   // id돌면서
-//               if(b.getPw().equals(inputPassword)) {
-//                  int index=Data.memberList.indexOf(b);
-//                                                // member.id가 로그인하면서 계속 돔
-//                  Data.memberList.remove(index);
-//                  membershipWithdrawalView.withDrawal();
-//                  loop = false;   
-//               } else {
-//                  membershipWithdrawalView.noneWithDrawal();
-//                  break;
-//               }
-//            }
-//            if(inputPassword.equals(businessPw)) {
-//               System.out.println("회원 탈퇴를 완료했습니다.");
-//               loop = false;
-//            } else {
-//               System.out.println("비밀번호가 일치하지 않습니다.");
-//               continue;
-//            }
-
-//         for(Member b : Data.memberList) {
-//            System.out.println(b.getName());
-//         }
--- BookATable/src/com/test/java/controller/StoreController.java
+++ BookATable/src/com/test/java/controller/StoreController.java
@@ -12,22 +12,29 @@ import com.test.java.repository.Data;
 import com.test.java.view.SignInView;
 import com.test.java.view.StoreView;
 
+/**
+ * 음식점을 검색하는 기능을 제공하는 StoreController 클래스
+ */
 public class StoreController {
 
     private StoreView storeView;
     private Store store;
     private ArrayList<Store> bookmarkedStores; // 즐겨찾기 목록
-
+    
+    /**
+     * StoreController의 생성자
+     */
     public StoreController() {
         this.storeView = new StoreView();
         this.bookmarkedStores = new ArrayList<>();
     }
 
     ArrayList<Store> searched = new ArrayList<>();
-    ArrayList<Menu> searchMenu = new ArrayList<>();
     ArrayList<Review> reviewCount = new ArrayList<>();
     
-
+    /**
+     * 상호명 및 메뉴명을 선택하여 해당 기능으로 갈 수 있는 run 메서드
+     */
     public void run() {
         boolean loop = true;
         while(loop) {
@@ -35,11 +42,11 @@ public class StoreController {
             int choice = storeView.get();
 
             switch(choice) {
-                case 0:
+                case 0:		// 이전 화면 이동
                     System.out.println("이전 화면으로 이동합니다.");
                     loop = false;
                     break;
-                case 1:
+                case 1:		// 상호명 검색
                     String storeName = storeView.getStoreName();
                     searched = searchStoreName(storeName);
                     if(searched.isEmpty()) {
@@ -49,7 +56,7 @@ public class StoreController {
                     storeView.show(searched);
                     loop = processSortingOption(searched);
                     break;
-                case 2:
+                case 2:		// 메뉴 검색
                     String menuName = storeView.getmenuName();
                     searched = searchMenuName(menuName);
                     if(searched.isEmpty()) {
@@ -59,12 +66,18 @@ public class StoreController {
                     storeView.show(searched);
                     loop = processSortingOption(searched);
                     break;
-                default:
+                default:	// 잘못된 값을 입력한 경우
                     System.out.println("잘못 입력하셨습니다. 0 ~ 2범위의 숫자를 입력하여 주세요.");
             }
         }
     }
-
+   
+    /**
+     * 별점 내림차순, 리뷰 갯수 내림차순, 상세페이지로 이동할 수 있는
+     * processSortingOption 메서드
+     * @param searched
+     * @return 
+     */
     private boolean processSortingOption(ArrayList<Store> searched) {
         boolean loop = true;
         while (loop) {
@@ -73,42 +86,45 @@ public class StoreController {
             ReservationController reservationController = new ReservationController();
 
             switch (sortingOption) {
-                case 0:
+                case 0:		// 이전 화면 이동
                     System.out.println("이전 화면으로 이동합니다.");
                     loop = false;
                     break;
-                case 1:
+                case 1:		// 별점 내림차순 정렬, 람다식으로 내림차순 정렬
                     Collections.sort(searched, (o1, o2) -> (int)(o2.getScore()*10 - o1.getScore()*10));
                     storeView.show(searched);
                     break;
-                case 2:
+                case 2:		// 리뷰 갯수 내림차순 정렬, 람다식으로 내림차순 정렬
                     Collections.sort(searched, (o1, o2) -> (o2.getReviewCount() - o1.getReviewCount()));
                     storeView.show(searched);
                     break;
-                case 3:
+                case 3:		// 상세페이지, false를 반환하는 경우 문장 종료
                     if (!detailPage()) {
                         return false;
                     }
                     break;
-                default:
+                default:	// 잘못된 값 입력 시 해당 내용 출력
                     System.out.println("잘못 입력하셨습니다. 0 ~ 3범위의 숫자를 입력하여 주세요.");
             }
         }
         return true;
     }
     
+    /**
+     * 상세페이지를 나타내는 detailPage 메서드
+     * @return 메뉴로 돌아가는 경우 > true, 아니면 false
+     */
     public boolean detailPage() {
         ReservationController reservationController = new ReservationController();
-        SignInController signInController = new SignInController();
         Scanner sc = new Scanner(System.in);
         storeView.bookmarkNreservation();
         int choice = sc.nextInt();
-
+        // 레벨 0 : 비회원, 레벨 1 : 일반 회원, 레벨 2 : 업체 회원
         switch(choice) {
-            case 0:
+            case 0:			// 이전 페이지 이동
                 storeView.backPage();
                 return true;
-            case 1:
+            case 1:			// 예약하기, 음식점 리스트의 인덱스 번호를 이용하여 사업자번호를 reservation에 전달하며 메서드 호출
                 if ((Member.level == 1)) {
                     storeView.show(searched);
                     storeView.reservationNumber();
@@ -122,13 +138,13 @@ public class StoreController {
                     storeView.show(searched);
                     storeView.noneMemberAvailable();
                     String input = storeView.getTxt();
-                    if(input.equals("y")) {
+                    if(input.equals("y")) {			// 회원가입 유무, Y를 누르면 초기 페이지 이동
                         return false;
-                    } else if(input.equals("n")) {
+                    } else if(input.equals("n")) {	// N을 누르면 이전 페이지 이동
                         storeView.backPage();
                         return true;
                     } else {
-                        storeView.inputError();
+                        storeView.inputError();		// 잘못된 값 입력 시 출력
                         return false;
                     }
                 }
@@ -150,7 +166,7 @@ public class StoreController {
         return true;
     }
 
-    private void bookmarkStore(ArrayList<Store> searched) {	// 즐겨찾기 등록
+    private void bookmarkStore(ArrayList<Store> searched) {	// 즐겨찾기 등록, 넘버링은 1부터 시작하므로 get으로 가져올 때 -1을 뺌
         Scanner scanner = new Scanner(System.in);
         storeView.show(searched);
         System.out.print("즐겨찾기에 추가할 음식점 번호를 선택하세요: ");
@@ -197,7 +213,11 @@ public class StoreController {
         }
     }
 
-
+    /**
+     * 특정 리뷰를 찾은 후 해당 음식점을 찾아 반환하는 searchReviewCount 메서드
+     * @param menu
+     * @return 
+     */
     private ArrayList<Store> searchReviewCount(String menu) {
         ArrayList<Store> tmp = new ArrayList<>();
 
@@ -214,7 +234,14 @@ public class StoreController {
         }
         return tmp;
     }
-
+    
+    /**
+     * Data의 메뉴리스트와 전달받은 menu가 같다면 menuList의 사업자번호를 licenseNumber 변수에 저장하고
+     * storeList의 요소들을 반복하여 store객체의 사업자번호와 licenseNumber가 같으면 tmp에 저장 후 반환하는
+     * searchMenuName 메서드
+     * @param menu
+     * @return tmp
+     */
     private ArrayList<Store> searchMenuName(String menu) {
         ArrayList<Store> tmp = new ArrayList<>();
 
@@ -230,6 +257,14 @@ public class StoreController {
         }
         return tmp;
     }
+    
+    /**
+     * 상호명으로 검색하는 searchStoreName 메서드
+     * equal 메서드가 아닌 contains 메서드를 사용하여 해당 키워드가 포함된
+     * 음식점을 모두 tmp 객체에 저장
+     * @param keyword
+     * @return tmp
+     */
 
     private ArrayList<Store> searchStoreName(String keyword) {
         ArrayList<Store> tmp = new ArrayList<>();

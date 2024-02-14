306e482b2ae5c2bf72e9697bfc858087e6c47864
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:55:44 +0900

    Update TableManagementView.java

--- BookATable/src/com/test/java/view/TableManagementView.java
+++ BookATable/src/com/test/java/view/TableManagementView.java
@@ -1,31 +1,41 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//테이블 관리 화면 출력하는 class
+
+/**
+ * 테이블 관리 화면을 출력하는 클래스
+ */
 public class TableManagementView {
-	//선택 메서드
-public void showChoice(){
-		
-		System.out.println("==========================================================================================");
-		System.out.println();
-		System.out.println("■           1. 테이블 정보 등록		■");
-		System.out.println("■           2. 테이블 정보 수정		■");
-		System.out.println("■           3. 테이블 삭제		  	■");
-		System.out.println("■           4. 예약 불가 테이블 지정	   	■");
-		System.out.println("■           5. 예약 불가 테이블 해제	   	■");
-		System.out.println("■           0. 이전 화면으로	    	■");
-		System.out.println();
-		System.out.println("==========================================================================================");
-		System.out.println();
-		System.out.print("선택(번호) : ");
-	}
-//선택한 번호 입력받는 메서드
-	public static int get() {
-		Scanner scan = new Scanner(System.in);
-		int temp = scan.nextInt();
-		scan.nextLine();
-		
-		return temp;
-	}
+    
+    /**
+     * 선택 메뉴를 출력하는 메서드
+     */
+    public void showChoice(){
+        System.out.println("==========================================================================================");
+        System.out.println();
+        System.out.println("■           1. 테이블 정보 등록          ■");
+        System.out.println("■           2. 테이블 정보 수정          ■");
+        System.out.println("■           3. 테이블 삭제              ■");
+        System.out.println("■           4. 예약 불가 테이블 지정       ■");
+        System.out.println("■           5. 예약 불가 테이블 해제       ■");
+        System.out.println("■           0. 이전 화면으로             ■");
+        System.out.println();
+        System.out.println("==========================================================================================");
+        System.out.println();
+        System.out.print("선택(번호) : ");
+    }
+    
+    /**
+     * 선택한 번호를 입력받는 메서드
+     * 
+     * @return 선택된 번호
+     */
+    public static int get() {
+        Scanner scan = new Scanner(System.in);
+        int temp = scan.nextInt();
+        scan.nextLine();
+        
+        return temp;
+    }
 
 }

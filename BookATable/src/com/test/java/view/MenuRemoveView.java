8daf2cdd13ce607c8ef8b58086ce19db8392f2e9
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:54:20 +0900

    주석

--- BookATable/src/com/test/java/view/MenuRemoveView.java
+++ BookATable/src/com/test/java/view/MenuRemoveView.java
@@ -1,21 +1,36 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//메뉴 삭제 화면 보여주는 class
+
+// 메뉴 삭제 화면을 보여주는 class
 public class MenuRemoveView {
-	
+    /**
+     * 삭제할 메뉴 이름을 입력받는 메서드
+     * 
+     * @return 삭제할 메뉴 이름
+     */
     public static String findMenuName() {
         Scanner scan = new Scanner(System.in);
         System.out.print("1. 어떤 메뉴를 삭제하시겠습니까? : ");
         return scan.nextLine();
     }
-    //메뉴 이름 입력 받는 메서드
+
+    /**
+     * 삭제할 메뉴의 이름을 입력받는 메서드
+     * 
+     * @return 삭제할 메뉴의 이름
+     */
     public static String getMenuName() {
         Scanner scan = new Scanner(System.in);
         System.out.print("1. 메뉴 이름 : ");
         return scan.nextLine();
     }
-    //삭제 확인 입력 받는 메서드
+
+    /**
+     * 삭제를 확인하는 입력을 받는 메서드
+     * 
+     * @return 삭제를 확인하는 입력값 ("y" 또는 "n")
+     */
     public static String removeCheck() {
         Scanner scan = new Scanner(System.in);
         String tmp = "";

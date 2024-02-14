3ca1b39105658aec8247e02a0760d792868dc258
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:53:50 +0900

    주석

--- BookATable/src/com/test/java/view/MenuEditView.java
+++ BookATable/src/com/test/java/view/MenuEditView.java
@@ -1,22 +1,37 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//메뉴 수정 화면 보여주는 class
+
+// 메뉴 수정 화면을 보여주는 class
 public class MenuEditView {
 
-	//메뉴 이름 입력 받는 메서드
+    /**
+     * 수정할 메뉴 이름을 입력받는 메서드
+     * 
+     * @return 수정할 메뉴 이름
+     */
     public static String findMenuName() {
         Scanner scan = new Scanner(System.in);
         System.out.print("1. 어떤 메뉴를 수정하시겠습니까? : ");
         return scan.nextLine();
     }
 
+    /**
+     * 수정할 메뉴의 새로운 이름을 입력받는 메서드
+     * 
+     * @return 새로운 메뉴 이름
+     */
     public static String getMenuName() {
         Scanner scan = new Scanner(System.in);
         System.out.print("1. 메뉴 이름 : ");
         return scan.nextLine();
     }
-    //메뉴 가격 입력 받는 메서드
+
+    /**
+     * 수정할 메뉴의 새로운 가격을 입력받는 메서드
+     * 
+     * @return 새로운 메뉴 가격
+     */
     public static int getMenuPrice() {
         Scanner scan = new Scanner(System.in);
         int temp = 0;

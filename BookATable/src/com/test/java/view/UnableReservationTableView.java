3781d942463d50fc783fc21f77d2841b5dacda73
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:56:12 +0900

    Update UnableReservationTableView.java

--- BookATable/src/com/test/java/view/UnableReservationTableView.java
+++ BookATable/src/com/test/java/view/UnableReservationTableView.java
@@ -1,22 +1,30 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//예약 불가 테이블 화면 출력 class
+
+/**
+ * 예약 불가 테이블 화면을 출력하는 클래스
+ */
 public class UnableReservationTableView {
-	//테이블 정원 입력 받는 메서드
-	public static int getTableCapacity() {
+    
+    /**
+     * 테이블 정원을 입력받는 메서드
+     * 
+     * @return 입력된 테이블 정원
+     */
+    public static int getTableCapacity() {
         Scanner scan = new Scanner(System.in);
         int temp = 0;
         boolean validInput = false;
         
         while (!validInput) {
-            System.out.print("1. 어떤 크기의 테이블을 예약불가하시겠습니까? : ");
+            System.out.print("1. 어떤 크기의 테이블을 예약 불가하시겠습니까? : ");
             if (scan.hasNextInt()) {
                 temp = scan.nextInt();
                 validInput = true;
             } else {
                 System.out.println("숫자를 입력하세요.");
-                scan.next(); // discard invalid input
+                scan.next(); // 잘못된 입력을 버림
             }
         }
         return temp;

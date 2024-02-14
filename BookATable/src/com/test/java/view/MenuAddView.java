d448c88a9de8f1de53c7c4a135df152717715102
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:53:36 +0900

    주석주석

--- BookATable/src/com/test/java/view/MenuAddView.java
+++ BookATable/src/com/test/java/view/MenuAddView.java
@@ -1,19 +1,31 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//메뉴 등록 화면 보여주는 class
+
+// 메뉴 등록 화면을 보여주는 class
 public class MenuAddView {
+
+    /**
+     * 메뉴 이름을 입력받는 메서드
+     * 
+     * @return 입력받은 메뉴 이름
+     */
     public static String getMenuName() {
         Scanner scan = new Scanner(System.in);
         System.out.print("1. 메뉴 이름 : ");
         return scan.nextLine();
     }
-    
+
+    /**
+     * 메뉴 가격을 입력받는 메서드
+     * 
+     * @return 입력받은 메뉴 가격
+     */
     public static int getMenuPrice() {
         Scanner scan = new Scanner(System.in);
         int temp = 0;
         boolean validInput = false;
-        
+
         while (!validInput) {
             System.out.print("2. 메뉴 가격 : ");
             if (scan.hasNextInt()) {

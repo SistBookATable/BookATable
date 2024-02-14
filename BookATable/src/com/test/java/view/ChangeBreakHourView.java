10cbef0d4df20d0e967aa9c54a7217985c531665
ctgombo <125354602+ctgombo@users.noreply.github.com>
2024-02-14 17:52:20 +0900

    주석주석

--- BookATable/src/com/test/java/view/ChangeBreakHourView.java
+++ BookATable/src/com/test/java/view/ChangeBreakHourView.java
@@ -1,42 +1,46 @@
 package com.test.java.view;
 
 import java.util.Scanner;
-//휴게시간 변경 화면을 보여주는 class
+
+// 휴게시간 변경 화면을 보여주는 class
 public class ChangeBreakHourView {
-	
-	
-	
-	//수정 할 요일 입력 받는 메서드
-	public static String getDay() {
-	      
-	      Scanner scan = new Scanner(System.in);
-	      String tmp = "";
-	      System.out.print("1. 수정 할 요일 : ");
-	      tmp = scan.nextLine();
-	      scan.close();
-	      return tmp;
-	   }
-	// 휴게 시작 시각 입력 받는 메서드
-	public static String getOpen() {
-	      
-	      Scanner scan = new Scanner(System.in);
-	      String tmp = "";
-	      System.out.print("2. 휴게 시작 시각 등록(00:00) : ");
-	      tmp = scan.nextLine();
-	      scan.close();
-	      return tmp;
-	   }
-	//휴게 종료 시각 입력 받는 메서드
-	public static String getClose() {
-	      
-	      Scanner scan = new Scanner(System.in);
-	      String tmp = "";
-	      System.out.print("3. 휴게 종료 시각 등록(00:00) : ");
-	      tmp = scan.nextLine();
-	      scan.close();
-	      return tmp;
-	   }
-
-	
-
+    
+    /**
+     * 수정할 요일을 입력받는 메서드
+     * 
+     * @return 입력받은 요일
+     */
+    public static String getDay() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. 수정할 요일 : ");
+        String day = scan.nextLine();
+        scan.close();
+        return day;
+    }
+    
+    /**
+     * 휴게 시작 시각을 입력받는 메서드
+     * 
+     * @return 입력받은 휴게 시작 시각
+     */
+    public static String getOpen() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("2. 휴게 시작 시각 등록(00:00) : ");
+        String open = scan.nextLine();
+        scan.close();
+        return open;
+    }
+    
+    /**
+     * 휴게 종료 시각을 입력받는 메서드
+     * 
+     * @return 입력받은 휴게 종료 시각
+     */
+    public static String getClose() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("3. 휴게 종료 시각 등록(00:00) : ");
+        String close = scan.nextLine();
+        scan.close();
+        return close;
+    }
 }
